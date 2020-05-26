c6b976c5b6055ba69ccd9825a8a510be524dedea
Raymond Gasper <raymond.gasper@enkochem.com>
2020-05-26 14:35:36 -0400

    strip leading and trailing quotes from url

--- run_black_on_git_diff_adds.sh
+++ run_black_on_git_diff_adds.sh
@@ -13,8 +13,9 @@ fi
 
 set -x
 
-echo -En ${GITHUB_EVENT_PATH}
 github_pr_url=`jq '.pull_request.url' ${GITHUB_EVENT_PATH}`
+# github pr url sometimes has leading and trailing quotes
+github_pr_url=`sed -e 's/^"//' -e 's/"$//' <<<"$github_pr_url"`
 github_diff=`curl --request GET --url ${github_pr_url} --header "authorization: Bearer ${GITHUB_TOKEN}" --header "Accept: application/vnd.github.v3.diff"`
 list_of_edited_files=`echo -En ${github_diff} | grep -E -- "\+\+\+ " | awk '{print $2}' | grep -Po -- "(?<=[ab]/).+(.py$)"`
 
