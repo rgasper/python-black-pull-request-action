e8836e9dca4abf6925cc8f0293b8e11eb4f66d5a
Raymond Gasper <raymond.gasper@enkochem.com>
2020-05-26 11:40:16 -0400

    130 char lines

--- run_black_on_git_diff_adds.sh
+++ run_black_on_git_diff_adds.sh
@@ -9,4 +9,4 @@ github_pr_url=`echo -En ${GITHUB_EVENT_PATH} | jq '.pull_request.url'`
 github_diff=`curl --request GET --url ${github_pr_url} --header "authorization: Bearer ${GITHUB_TOKEN}" --header "Accept: application/vnd.github.v3.diff"`
 list_of_edited_files=`echo -En ${github_diff} | grep -E -- "\+\+\+ " | awk '{print $2}' | grep -Po -- "(?<=[ab]/).+(.py$)"`
 
-black ${list_of_edited_files}
+black --line_length 130 ${list_of_edited_files}
